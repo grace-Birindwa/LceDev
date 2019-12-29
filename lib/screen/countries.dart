9a5142e53d972ac26f340e324b534cb6c2ae4b44
grace birindwa <gracebrindwa@gmail.com>
2019-12-29 19:09:37 +0100

    add of add in future list

--- lib/screen/countries.dart
+++ lib/screen/countries.dart
@@ -14,14 +14,21 @@ class AllCountries extends StatefulWidget {
 
 class _AllCountriesState extends State<AllCountries> {
   var response;
- void getcountries() async {
+  Future<List> lcountries;
+ Future<List> getcountries() async {
      response = await Dio().get('https://restcountries.eu/rest/v2/all');
-     print(response.data);
+     return response.data;
+  }
+  @override
+  void initState() {
+    lcountries = getcountries();
+    
+    super.initState();
   }
   @override
 
   Widget build(BuildContext context) {
-   
+  
     return Container(
        child: Scaffold(
          appBar: AppBar(
@@ -32,36 +39,15 @@ class _AllCountriesState extends State<AllCountries> {
            padding: EdgeInsets.all(10),
            // we create a listview with children cause it could holp more than one widgets
            
-            child: ListView(children: <Widget>[
-               GestureDetector(
-                 onTap: (){
-                   Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Country('DRCongo')
-                   ));
-                 },
-              child: Card(
-             elevation: 10,
-             child: Padding(
-               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-               child: Text("DRCongo"),
-             ),
-           ),
-               ),
-               GestureDetector(
-                
-                 onTap: (){
-                   Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Country('Rwanda')
-                   ));
-                    getcountries();
-                 },
-              child: Card(
-             elevation: 10,
-             child: Padding(
-               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-               child: Text("Rwanda"),
-             ),
-           ),
-               ),
-            ],)
+            child: FutureBuilder<List>(
+              future: lcountries,
+              builder: (BuildContext context, AsyncSnapshot<List> snapshot ){
+                if(snapshot.hasData){
+                  print(snapshot.data);
+                  return Text('hello');
+                }
+                return null;
+              },)
          )
        )
     );
